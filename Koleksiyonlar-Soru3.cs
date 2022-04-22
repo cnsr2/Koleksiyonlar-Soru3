16d07545ee5a9c5ecec88ad59f9889194bf678a5
Emre Can Sari <cansarrr2@gmail.com>
2022-04-22 22:29:10 +0300

    first commit

---
+++ Koleksiyonlar-Soru3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cümlenizi Giriniz.");
+            string cümle = Console.ReadLine();
+            string[] cümledizisi = new string[cümle.Count()];
+            //string[] harfler = new string[8];
+            ArrayList harfler = new ArrayList();
+            foreach (var item in cümle)
+            {
+                if (item == 'ü' || item == 'u' || item == 'ö' || item == 'o' || item == 'i'|| item == 'ı' || item == 'e'|| item == 'a')
+                {
+                    harfler.Add(item.ToString());
+                }
+            }
+            harfler.Sort();
+            Console.WriteLine("Cümlenizdeki Sesli Harfler : ");
+            foreach (var item in harfler)
+                {
+                    Console.WriteLine(item);
+                }
+        }
+    }
